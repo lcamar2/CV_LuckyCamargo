90389068b22869ce4d3dc4ef380b7846a589992c
Lucky Camargo <lcamar2@uic.edu>
2024-02-29 18:31:32 -0600

    updated file

--- r/data.r
+++ r/data.r
@@ -20,13 +20,11 @@ certifications <- tribble(
 
 skills <- tribble(
     ~area, ~skills,
-    "Programming Languages", "BASH, Batch, CCL, DAX, JavaScript, Python, R, T-SQL, PL-SQL, VBA",
-    "Markup Languages", "CSS, HTML, LaTeX, Markdown, RMarkdown, SASS, Groff/Troff",
-    "Data Interchange Formats", "CSV, JSON, XML, Feather",
-    "Other Languages", "YAML",
+    "Programming Languages", "R",
+    "Markup Languages", "Markdown, RMarkdown",
     "Version Control", "Git",
-    "Text Editors", "Discern Visual Developer, RStudio, SQL Server Management Studio, VIM, Visual Studio, Visual Studio Code",
-    "Microsoft Office", "Excel, Outlook, OneNote, PowerBI, PowerPoint, Visio, Word"
+    "Text Editors", "RStudio",
+    "Microsoft Office", "Excel, Outlook, PowerPoint, Word"
 )
 
 honors <- tribble(
@@ -37,8 +35,8 @@ honors <- tribble(
 
 edu <- tribble(
     ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
-    "Western Governors University", 2020, NA, "Bachelors Data Analytics & Data Management", "Online",NA,
-    "American Council on Education - Statistics.com", 2019, 2019, "Statistics I & II", "Online",NA,
+    "University of Illinois", 2021, NA, "Master of Science Civic Analytics", "Chicago - Illinois",NA,
+    "Northeastern Illinois University", 2017, 2020, "Bachelor of Arts Economics, minor Philosophy", "Chicago - Illinois",NA,
     "Cosumnes River College", 2012, 2015, "Associate of Art (A.A.) Physical Education Teaching and Coaching", "Sacramento - California", "Phi Theta Kappa Academic Honor Society",
     "Cosumnes River College", 2012, 2015, "Associate of Science (A.S.) Liberal Arts: Math and Science", "Sacramento - California", "Phi Theta Kappa Academic Honor Society",
     "San Joaquin Delta College", 2011, 2011, "Pre-Calculus", "Stockton - California",NA,
